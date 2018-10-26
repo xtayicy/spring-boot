4e096bb2e646ebf26899382d470384bca88dd560
harry <xtayicy@gmail.com>
2018-10-26 17:22:30 +0800

    spring-boot-actuator-custom-security

--- spring-boot-actuator-custom-security/src/main/java/harry/SpringBoot/actuator/custom/security/controller/HomeController.java
+++ spring-boot-actuator-custom-security/src/main/java/harry/SpringBoot/actuator/custom/security/controller/HomeController.java
@@ -1,10 +1,6 @@
 package harry.SpringBoot.actuator.custom.security.controller;
 
-import java.util.Date;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,15 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String home(Map<String, Object> model) {
-		model.put("message", "Hello");
-		model.put("title", "Hello Home");
-		model.put("date", new Date());
-		
-		return "home";
-	}
-	
 	@RequestMapping("/foo")
 	public String foo() {
 		throw new RuntimeException("Expected exception in controller");
