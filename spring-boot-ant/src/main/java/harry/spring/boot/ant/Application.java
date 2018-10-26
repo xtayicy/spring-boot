75d0657cc9a4b3a279286365b9cebf7b8158478b
harry <xtayicy@gmail.com>
2018-10-26 10:20:16 +0800

    spring-boot-ant

--- spring-boot-ant/src/main/java/harry/spring/boot/ant/Application.java
+++ spring-boot-ant/src/main/java/harry/spring/boot/ant/Application.java
@@ -1,5 +1,6 @@
 package harry.spring.boot.ant;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Spring Boot Ant Example");
 	}
 }
