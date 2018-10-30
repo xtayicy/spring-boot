626d0e52e54bfd5c817f584ffed08df608de1b32
harry <xtayicy@gmail.com>
2018-10-30 17:46:41 +0800

    spring-boot-web-jsp

---
+++ spring-boot-web-jsp/src/main/java/harry/spring/boot/web/jsp/Application.java
@@ -0,0 +1,16 @@
+package harry.spring.boot.web.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author harry
+ *
+ */
+@SpringBootApplication
+public class Application{
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ spring-boot-web-jsp/src/main/java/harry/spring/boot/web/jsp/controller/HelloController.java
@@ -0,0 +1,33 @@
+package harry.spring.boot.web.jsp.controller;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author harry
+ *
+ */
+@Controller
+public class HelloController {
+	@Value("${application.message:Hello World}")
+	private String message = "Hello World";
+	
+	@GetMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+		model.put("message", this.message);
+		
+		return "welcome";
+	}
+
+	@RequestMapping("/foo")
+	public String foo(Map<String, Object> model) {
+		throw new RuntimeException("Foo");
+	}
+}
