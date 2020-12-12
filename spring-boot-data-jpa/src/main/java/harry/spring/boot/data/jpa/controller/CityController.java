be64ca78edd1945a7e2eb0157d3a03304d2a5ecc
xtayicy <xtayicy@gmail.com>
2020-12-12 23:00:02 +0800

    spring-boot

--- spring-boot-data-jpa/src/main/java/harry/spring/boot/data/jpa/controller/CityController.java
+++ spring-boot-data-jpa/src/main/java/harry/spring/boot/data/jpa/controller/CityController.java
@@ -1,26 +1,23 @@
 package harry.spring.boot.data.jpa.controller;
 
-import harry.spring.boot.data.jpa.service.CityService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import harry.spring.boot.data.jpa.service.CityService;
 
 /**
  * 
  * @author harry
  *
  */
-@Controller
+@RestController
 public class CityController {
 	@Autowired
 	private CityService cityService;
 	
 	@GetMapping("/")
-	@ResponseBody
-	@Transactional(readOnly = true)
+	//@Transactional(readOnly = true)
 	public String getCityName() {
 		return this.cityService.getCity("Bath", "UK").getName();
 	}
